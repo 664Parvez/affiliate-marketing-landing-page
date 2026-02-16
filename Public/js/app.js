24ba60e69bec047fc654ec3ec1d5463a37b0533d
664Parvez <parvezrabbi664@gmail.com>
2026-02-16 19:15:46 +0600

    update3

--- Public/js/app.js
+++ Public/js/app.js
@@ -29,3 +29,40 @@ copyright.innerText = copyDate
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     }
+
+
+// sidebar
+const sidebar_info = document.querySelector(".sidebar_info")
+const open_sidebar = document.getElementById("open_sidebar")
+
+open_sidebar.addEventListener("click", (e) => {
+    sidebar_info.style.right = 0
+
+    e.preventDefault()
+})
+
+const close_sidebar = document.getElementById("close_sidebar")
+
+close_sidebar.addEventListener("click", (e) => {
+    sidebar_info.style.right = "-400px"
+    sidebar_info.style.transition = ".5s"
+
+    e.preventDefault()
+})
+
+
+// Cursor
+window.addEventListener("mousemove", (e)=>{
+    let cursor = document.getElementById("cursor");
+    setTimeout(()=>{
+        cursor.style.top = `${e.clientY}px`;
+        cursor.style.left = `${e.clientX}px`;
+    },50)
+
+    if(e.target.id === "box") {
+        cursor.classList.add("active");
+    }
+    else {
+        cursor.classList.remove("active");
+    }
+})
