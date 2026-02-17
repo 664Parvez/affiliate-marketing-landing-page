4e3d7f35680adb7ca8f50fc4dda6ef1b9f3652a0
664Parvez <parvezrabbi664@gmail.com>
2026-02-17 09:49:11 +0600

    update2

--- Public/js/app.js
+++ Public/js/app.js
@@ -51,19 +51,19 @@ close_sidebar.addEventListener("click", (e) => {
 })
 
 
-// Cursor
-window.addEventListener("mousemove", (e)=>{
-    let cursor = document.getElementById("cursor");
-    setTimeout(()=>{
-        cursor.style.top = `${e.clientY}px`;
-        cursor.style.left = `${e.clientX}px`;
-    },50)
+// // Cursor
+// window.addEventListener("mousemove", (e)=>{
+//     let cursor = document.getElementById("cursor");
+//     setTimeout(()=>{
+//         cursor.style.top = `${e.clientY}px`;
+//         cursor.style.left = `${e.clientX}px`;
+//     },50)
 
-    if(e.target.id === "box") {
-        cursor.classList.add("active");
-    }
-    else {
-        cursor.classList.remove("active");
-    }
-})
+//     if(e.target.id === "box") {
+//         cursor.classList.add("active");
+//     }
+//     else {
+//         cursor.classList.remove("active");
+//     }
+// })
 
